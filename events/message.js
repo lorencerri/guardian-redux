984b89d1fd0b40abcf56e073d0b0e0d13560bd91
Loren Cerri <truexpixels@gmail.com>
2019-01-27 13:14:30 -0800

    Create message.js

---
+++ events/message.js
@@ -0,0 +1,19 @@
+exports.run = (client, message) => {
+    
+    // Check for prefix
+    if (!message.content.startsWith(client.prefix)) return;
+    
+    // Declare & Initialize Variables
+    const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    // Return Statements
+    if (message.author.bot || !message.channel.guild || !message.guild) return;
+    if (!client.commands.has(cmd)) return;
+    if (!client.main && !['limits'].includes(cmd)) return;
+    
+    // Run Command
+    const command = client.commands.get(cmd);
+    command.exec(message, args);
+    
+}
