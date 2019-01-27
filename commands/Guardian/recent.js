17a11560e5eb1b3f1344cd8be230d5c35b0d7220
Loren Cerri <truexpixels@gmail.com>
2019-01-27 13:10:11 -0800

    Create recent.js

---
+++ commands/Guardian/recent.js
@@ -0,0 +1,45 @@
+const Command = require('../../structures/Command');
+const { MessageEmbed } = require('discord.js');
+
+class Recent extends Command {
+    constructor(client) {
+        super({
+            name: 'recent'
+        });
+
+        this.client = client;
+    }
+
+    exec(message, args) {
+
+        // Create Embed
+        const embed = new MessageEmbed()
+            .setColor(this.client.EMBED_COLOR);
+            
+        // Variables
+        let guild = message.guild;
+        let limits = guild.limits;
+        let keys = Object.keys(limits);
+        
+        // Loop Through Keys [Excluding Pings]
+        for (var i = 0; i < keys.length; i++) {
+            if (keys[i] === 'pings') continue;
+            
+            // Variables
+            let msg = '';
+            let data = this.client.db.get(`${keys[i]}_${guild.id}`).slice(-10).reverse();
+            
+            // Loop through data
+            for (var x in data) msg += `\`${this.client.parseTime(data[x].timestamp)}\` | ${this.client.users.get(data[x].executor.id)} ${data[x].type} ${data[x].target.tag}\n`
+            embed.addField(`${this.client.camelToString(keys[i])} (${data.length})`, msg);
+            
+        }
+        
+        // Send Message
+        message.channel.send(embed);
+        
+    }
+
+}
+
+module.exports = Recent;
