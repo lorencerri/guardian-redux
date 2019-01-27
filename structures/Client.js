c5aa71245cc18839f2b3b109164bb2327d2b3b59
Loren Cerri <truexpixels@gmail.com>
2019-01-27 13:15:41 -0800

    Create Client.js

---
+++ structures/Client.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const Enmap = require('enmap');
+const db = new Enmap({ name: 'db' });
+const parsems = require('parse-ms');
+
+require('./Guild');
+
+class Base extends Discord.Client {
+    constructor(...args) {
+        super(...args);
+        
+        // General
+        this.commands = new Discord.Collection();
+        this.aliases = new Discord.Collection();
+        this.CommandHandler = new (require('./CommandHandler'))(this);
+        this.EventHandler = new (require('./EventHandler'))(this);
+        
+        // Modules
+        this.db = db;
+
+        // Constants
+        this.prefix = 'g!';
+        this.EMBED_COLOR = 0x7289DA;
+        this.COLOR_RED = 0xf04747;
+        
+    }
+    
+    run(settings) {
+        
+        this.main = !!(settings || {}).main;
+        this.evalID = (settings || {}).evalID || null;
+        
+        this.CommandHandler.load();
+        this.EventHandler.load();
+        
+    }
+    
+    camelToString(s) {
+        return s.charAt(0).toUpperCase() + s.replace(/([A-Z])/g, ' $1').trim().substr(1);
+    }
+    
+    parseTime(ms, { fromNow = false, includeSeconds = false, base = '' } = {}) {
+        
+        let obj = (fromNow ? parsems(ms) : parsems(Date.now() - ms));
+
+        for (var i in obj) {
+            if (obj[i] === 0 || ['milliseconds', 'microseconds', 'nanoseconds'].includes(i) || (!includeSeconds && i === 'seconds')) continue;
+            base += `${obj[i]} ${(obj[i] === 1 ? i.slice(0, -1) : i)} `;
+        }
+        
+        return (!base ? 'Just now' : base + 'ago');
+        
+    }
+    
+}
+
+module.exports = Base;
