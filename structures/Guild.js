66f8d26adb2e8cd6900ab3c0b70b95b89a6719a1
TrueXPixels <truexpixels@gmail.com>
2019-01-28 20:11:34 +0100

    Bugfix

--- structures/Guild.js
+++ structures/Guild.js
@@ -99,7 +99,7 @@ Structures.extend('Guild', Guild => {
             // Actions
             if (type === 'pings') {
                 let role = this.roles.get(this.config.mutedRole.data);
-                if (member.roles.find(r => r.name === role.name)) return console.log('Already has muted role, force exiting...');
+                if (role && member.roles.find(r => r.name === role.name)) return console.log('Already has muted role, force exiting...');
                 else if (role) {
                     member.roles.add(role);
                     embed.setFooter('Automatic Action: Added Muted Role');
